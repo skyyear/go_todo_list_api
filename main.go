4b1ef1355d7d730e6646d2b8b0f4d10fd514c9c0
skyyear <94643832+skyyear@users.noreply.github.com>
2023-08-19 18:32:23 -0700

    add env for ssl_mode

--- main.go
+++ main.go
@@ -244,11 +244,12 @@ func main() {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("SSL_MODE")
 
 	// create connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		"password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
 
 	fmt.Println(psqlInfo)
 
